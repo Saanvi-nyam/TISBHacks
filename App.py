d37ed5093ae4fd0f7b89d98cbfefe5af91fe4457
Saanvi-nyam <130659292+Saanvi-nyam@users.noreply.github.com>
2024-02-29 19:39:06 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+def main():
+    st.set_page_config(page_title="Ask your PDF")
+    load_dotenv()
+    st.header("Ask your PDF 💬")
+    
+    # sidebar
+    with st.sidebar:
+        st.title('LLM Chat App')
+        st.markdown('''
+        ## About
+        This app is an LLM-powered chatbot built using:
+        - [Streamlit](https://streamlit.io/)
+        - [LangChain](https://python.langchain.com/)
+        - [OpenAI](https://platform.openai.com/docs/models) LLM Model
+
+        ''')
+        add_vertical_space(5)
+        st.write('Made with love by Team WGS')
+
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+      
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+        # show user input
+        user_question = st.text_input("Ask a question about your PDF:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+        
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+           
+            st.write(response)
+
+if __name__ == '__main__':
+    main()
